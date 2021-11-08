74da0e645c84932cc502cab60fb5fa84be4160b2
Ephiret <60055617+Ephiret@users.noreply.github.com>
2021-11-08 09:44:25 +0530

    Added heart and diabetes fields

--- src/components/Diabetes/index.js
+++ src/components/Diabetes/index.js
@@ -1,7 +1,103 @@
 import React from "react";
+import { Col, FloatingLabel, Form, Row, Button } from "react-bootstrap";
+import FormComponent from "../Form/form";
+import { useState } from "react";
 
 function Diabetes() {
-  return <div>Hi diabetes</div>;
+  const [features, setFeatures] = useState([]);
+  const [Pregnancies, setPregnancies] = useState(0);
+  const [Glucose, setGlucose] = useState(0);
+  const [BloodPressure, setBloodPressure] = useState(0);
+  const [SkinThickness, setSkinThickness] = useState(0);
+  const [Insulin, setInsulin] = useState(0);
+  const [BMI, setBMI] = useState(0);
+  const [DiabetesPedigreeFunction, setDiabetesPedigreeFunction] = useState(0);
+  const [Age, setAge] = useState(0);
+  
+  function handleClick() {
+    console.log(
+      Pregnancies + " " + Insulin 
+    );
+  }
+  function changePregnancies(a) {
+    setPregnancies(a);
+  }
+  function changeGlucose(a) {
+    setGlucose(a);
+  }
+  function changeBloodPressure(a) {
+    setBloodPressure(a);
+  }
+  function changeSkinThickness(a) {
+    setSkinThickness(a);
+  }
+  function changeInsulin(a) {
+    setInsulin(a);
+  }
+  function changeBMI(a) {
+    setBMI(a);
+  }
+  function changeDiabetesPedigreeFunction(a) {
+    setDiabetesPedigreeFunction(a);
+  }
+  function changeAge(a) {
+    setAge(a);
+  }
+  return (
+    <div className="container">
+      <h3 className="mb-4">Please fill the details</h3>
+
+      <Form>
+        <Row>
+          <Col md={6} className="px-0">
+            <FormComponent
+              id="fromPregnancies"
+              featureName="Pregnancies"
+              func={changePregnancies}
+            ></FormComponent>
+            <FormComponent
+              id="fromGlucose"
+              featureName="Glucose"
+              func={changeGlucose}
+            ></FormComponent>
+            <FormComponent
+              id="fromBloodPressure"
+              featureName="Blood Pressure"
+              func={changeBloodPressure}
+            ></FormComponent>
+            <FormComponent
+              id="fromSkinThickness"
+              featureName="Skin Thickness:"
+              func={changeSkinThickness}
+            ></FormComponent>
+            <FormComponent
+              id="fromInsulin"
+              featureName="Insulin"
+              func={changeInsulin}
+            ></FormComponent>
+            <FormComponent
+              id="fromBMI"
+              featureName="BMI"
+              func={changeBMI}
+            ></FormComponent>
+            <FormComponent
+              id="fromDiabetesPedigreeFunction"
+              featureName="Diabetes Pedigree Function"
+              func={changeDiabetesPedigreeFunction}
+            ></FormComponent>
+            <FormComponent
+              id="fromAge"
+              featureName="Age"
+              func={changeAge}
+              ></FormComponent>
+          </Col>
+        </Row>
+        <Button variant="warning" onClick={handleClick}>
+          SUBMIT
+        </Button>{" "}
+      </Form>
+    </div>
+  );
 }
 
 export default Diabetes;
--- src/components/Heart/index.js
+++ src/components/Heart/index.js
@@ -1,7 +1,112 @@
 import React from "react";
+import { Col, FloatingLabel, Form, Row, Button } from "react-bootstrap";
+import FormComponent from "../Form/form";
+import { useState } from "react";
 
 function Heart() {
-  return <div>Hi heart</div>;
+  const [features, setFeatures] = useState([]);
+  const [Sex, setSex] = useState(0);
+  const [Chestpaintype, setChestpaintype] = useState(0);
+  const [Trestbps, setTrestbps] = useState(0);
+  const [SerumCholestoral, setSerumCholestoral] = useState(0);
+  const [RestECG, setRestECG] = useState(0);
+  const [Thalach, setThalach] = useState(0);
+  const [Age, setAge] = useState(0);
+  const [Slope, setSlope] = useState(0);
+  const [Thal, setThal] = useState(0);
+  
+  function handleClick() {
+    console.log(
+      Sex + " " + RestECG 
+    );
+  }
+  function changeSex(a) {
+    setSex(a);
+  }
+  function changeChestpaintype(a) {
+    setChestpaintype(a);
+  }
+  function changeTrestbps(a) {
+    setTrestbps(a);
+  }
+  function changeSerumCholestoral(a) {
+    setSerumCholestoral(a);
+  }
+  function changeRestECG(a) {
+    setRestECG(a);
+  }
+  function changeThalach(a) {
+    setThalach(a);
+  }
+  function changeAge(a) {
+    setAge(a);
+  }
+  function changeSlope(a) {
+    setSlope(a);
+  }
+  function changeThal(a) {
+    setThal(a);
+  }
+  return (
+    <div className="container">
+      <h3 className="mb-4">Please fill the details</h3>
+
+      <Form>
+        <Row>
+          <Col md={6} className="px-0">
+            <FormComponent
+              id="fromAge"
+              featureName="Age"
+              func={changeAge}
+            ></FormComponent>
+            <FormComponent
+              id="fromSex"
+              featureName="Sex"
+              func={changeSex}
+            ></FormComponent>
+            <FormComponent
+              id="fromChestpaintype"
+              featureName="Chest pain type"
+              func={changeChestpaintype}
+            ></FormComponent>
+            <FormComponent
+              id="fromTrestbps"
+              featureName="Trest BPS"
+              func={changeTrestbps}
+            ></FormComponent>
+            <FormComponent
+              id="fromSerumCholestoral"
+              featureName="Serum Cholestrol (in mg/dl) "
+              func={changeSerumCholestoral}
+            ></FormComponent>
+            <FormComponent
+              id="fromRestECG"
+              featureName="RestECG"
+              func={changeRestECG}
+            ></FormComponent>
+            <FormComponent
+              id="fromThalach"
+              featureName="Thalach"
+              func={changeThalach}
+            ></FormComponent>
+            <FormComponent
+              id="fromSlope"
+              featureName="Slope"
+              func={changeSlope}
+            ></FormComponent>
+            <FormComponent
+              id="fromTHal"
+              featureName="Thal"
+              func={changeThal}
+            ></FormComponent>
+          </Col>
+        </Row>
+        <Button variant="warning" onClick={handleClick}>
+          SUThalachT
+        </Button>{" "}
+      </Form>
+    </div>
+  );
 }
 
 export default Heart;
