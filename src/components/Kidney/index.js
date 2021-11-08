1e96026b02f939104a761a51ce4dd210a6be27f7
Ephiret <60055617+Ephiret@users.noreply.github.com>
2021-11-08 16:14:17 +0530

    complete diseases dashboard

--- src/components/Kidney/index.js
+++ src/components/Kidney/index.js
@@ -1,7 +1,138 @@
 import React from "react";
+import { Col, FloatingLabel, Form, Row, Button } from "react-bootstrap";
+import FormComponent from "../Form/form";
+import { useState } from "react";
 
 function Kidney() {
-  return <div>Hi Kidney</div>;
-}
+  const [features, setFeatures] = useState([]);
+  const [Age, setAge] = useState(0);
+  const [BP, setBP] = useState(0);
+  const [AL, setAL] = useState(0);
+  const [PCC, setPCC] = useState(0);
+  const [BGR, setBGR] = useState(0);
+  const [BU, setBU] = useState(0);
+  const [SC, setSC] = useState(0);
+  const [HEMO, setHEMO] = useState(0);
+  const [PCV, setPCV] = useState(0);
+  const [HTN, setHTN] = useState(0);
+  const [DM, setDM] = useState(0);
+  const [Appet, setAppet] = useState(0);
+  
+  function handleClick() {
+    console.log(
+      //Sex + " " + RestECG 
+    );
+  }
+  function changeAge(a) {
+    setAge(a);
+  }
+  function changeBP(a) {
+    setBP(a);
+  }
+  function changeAL(a) {
+    setAL(a);
+  }
+  function changePCC(a) {
+    setPCC(a);
+  }
+  function changeBGR(a) {
+    setBGR(a);
+  }
+  function changeBU(a) {
+    setBU(a);
+  }
+  function changeSC(a) {
+    setSC(a);
+  }
+  function changeHEMO(a) {
+    setHEMO(a);
+  }
+  function changePCV(a) {
+    setPCV(a);
+  }
+  function changeHTN(a) {
+    setHTN(a);
+  }
+  function changeDM(a) {
+    setDM(a);
+  }
+  function changeAppet(a) {
+    setAppet(a);
+  }
+  return (
+    <div className="container">
+      <h3 className="mb-4">Please fill the details</h3>
 
+      <Form>
+        <Row>
+          <Col md={6} className="px-0">
+            <FormComponent
+              id="fromAge"
+              featureName="Age"
+              func={changeAge}
+            ></FormComponent>
+            <FormComponent
+              id="fromBP"
+              featureName="BP"
+              func={changeBP}
+            ></FormComponent>
+            <FormComponent
+              id="fromAL"
+              featureName="AL"
+              func={changeAL}
+            ></FormComponent>
+            <FormComponent
+              id="fromPCC"
+              featureName="PCC"
+              func={changePCC}
+            ></FormComponent>
+            <FormComponent
+              id="fromBGR"
+              featureName="BGR"
+              func={changeBGR}
+            ></FormComponent>
+            <FormComponent
+              id="fromBU"
+              featureName="BU"
+              func={changeBU}
+            ></FormComponent>
+            <FormComponent
+              id="fromSC"
+              featureName="SC"
+              func={changeSC}
+            ></FormComponent>
+            <FormComponent
+              id="fromHEMO"
+              featureName="HEMO"
+              func={changeHEMO}
+            ></FormComponent>
+            <FormComponent
+              id="fromPCV"
+              featureName="PCV"
+              func={changePCV}
+            ></FormComponent>
+            <FormComponent
+              id="fromHTN"
+              featureName="HTN"
+              func={changeHTN}
+            ></FormComponent>
+            <FormComponent
+              id="fromDM"
+              featureName="DM"
+              func={changeDM}
+            ></FormComponent>
+            <FormComponent
+              id="fromAppet"
+              featureName="Appet"
+              func={changeAppet}
+            ></FormComponent>
+          </Col>
+        </Row>
+        <Button variant="warning" onClick={handleClick}>
+          SUThalachT
+        </Button>{" "}
+      </Form>
+    </div>
+  );
+}
 export default Kidney;
--- src/components/Liver/index.js
+++ src/components/Liver/index.js
@@ -1,7 +1,120 @@
 import React from "react";
+import { Col, FloatingLabel, Form, Row, Button } from "react-bootstrap";
+import FormComponent from "../Form/form";
+import { useState } from "react";
 
 function Liver() {
-  return <div>Hi Liver</div>;
-}
+  const [features, setFeatures] = useState([]);
+  const [Sex, setSex] = useState(0);
+  const [Total_Bilirubin, setTotal_Bilirubin] = useState(0);
+  const [Direct_Bilirubin, setDirect_Bilirubin] = useState(0);
+  const [Alkaline_Phosphotase, setAlkaline_Phosphotase] = useState(0);
+  const [Alamine_Aminotransferase, setAlamine_Aminotransferase] = useState(0);
+  const [Aspartate_Aminotransferase, setAspartate_Aminotransferase] = useState(0);
+  const [Age, setAge] = useState(0);
+  const [Total_Protiens, setTotal_Protiens] = useState(0);
+  const [Albumin, setAlbumin] = useState(0);
+  const [Albumin_and_Globulin_Ratio, setAlbumin_and_Globulin_Ratio] = useState(0);
+  
+  function handleClick() {
+    console.log(
+      Sex + " " + Alamine_Aminotransferase 
+    );
+  }
+  function changeSex(a) {
+    setSex(a);
+  }
+  function changeTotal_Bilirubin(a) {
+    setTotal_Bilirubin(a);
+  }
+  function changeDirect_Bilirubin(a) {
+    setDirect_Bilirubin(a);
+  }
+  function changeAlkaline_Phosphotase(a) {
+    setAlkaline_Phosphotase(a);
+  }
+  function changeAlamine_Aminotransferase(a) {
+    setAlamine_Aminotransferase(a);
+  }
+  function changeAspartate_Aminotransferase(a) {
+    setAspartate_Aminotransferase(a);
+  }
+  function changeAge(a) {
+    setAge(a);
+  }
+  function changeTotal_Protiens(a) {
+    setTotal_Protiens(a);
+  }
+  function changeAlbumin(a) {
+    setAlbumin(a);
+  }
+  function changeAlbumin_and_Globulin_Ratio(a) {
+    setAlbumin_and_Globulin_Ratio(a);
+  }
+  return (
+    <div className="container">
+      <h3 className="mb-4">Please fill the details</h3>
 
+      <Form>
+        <Row>
+          <Col md={6} className="px-0">
+            <FormComponent
+              id="fromAge"
+              featureName="Age"
+              func={changeAge}
+            ></FormComponent>
+            <FormComponent
+              id="fromSex"
+              featureName="Sex"
+              func={changeSex}
+            ></FormComponent>
+            <FormComponent
+              id="fromTotal_Bilirubin"
+              featureName="Total Bilirubin"
+              func={changeTotal_Bilirubin}
+            ></FormComponent>
+            <FormComponent
+              id="fromDirect_Bilirubin"
+              featureName="Direct Bilirubin"
+              func={changeDirect_Bilirubin}
+            ></FormComponent>
+            <FormComponent
+              id="fromAlkaline_Phosphotase"
+              featureName="Alkaline Phosphotase"
+              func={changeAlkaline_Phosphotase}
+            ></FormComponent>
+            <FormComponent
+              id="fromAlamine_Aminotransferase"
+              featureName="Alamine Aminotransferase"
+              func={changeAlamine_Aminotransferase}
+            ></FormComponent>
+            <FormComponent
+              id="fromAspartate_Aminotransferase"
+              featureName="Aspartate Aminotransferase"
+              func={changeAspartate_Aminotransferase}
+            ></FormComponent>
+            <FormComponent
+              id="fromTotal_Protiens"
+              featureName="Total Protiens"
+              func={changeTotal_Protiens}
+            ></FormComponent>
+            <FormComponent
+              id="fromAlbumin"
+              featureName="Albumin"
+              func={changeAlbumin}
+            ></FormComponent>
+            <FormComponent
+              id="fromAlbumin_and_Globulin_Ratio"
+              featureName="Albumin and Globulin Ratio"
+              func={changeAlbumin_and_Globulin_Ratio}
+            ></FormComponent>
+          </Col>
+        </Row>
+        <Button variant="warning" onClick={handleClick}>
+          SUBMIT
+        </Button>{" "}
+      </Form>
+    </div>
+  );
+}
 export default Liver;
